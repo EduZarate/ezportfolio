ce80d73ffafbad9cafa56c2284681b2c0937b5e0
Eduardo <zaratedu@gmail.com>
2023-03-12 17:04:35 -0300

    Enviar mensaje desde el formulario

---
+++ enviar.php
@@ -0,0 +1,24 @@
+<?php
+
+// LLamando a los campos
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+
+$destinatario = "zaratedu@gmail.com"
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $email \n"
+$carta .= "Mensaje: $mensaje";
+
+// Enviando mensaje
+
+mail($destinatario, $asunto, $carta)
+
+header("Location:index.html");
+
+?>
